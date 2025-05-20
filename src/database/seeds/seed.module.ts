8eb1a3746b8d1be2c9c368a7e93159fb0ca27cf9
Khady GUEYE <42519251+kasumi993@users.noreply.github.com>
2025-05-21 00:36:55 +0200

    fix seed module

--- src/database/seeds/seed.module.ts
+++ src/database/seeds/seed.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SeedService } from './seed.service';
 import { User } from '../../users/entities/user.entity';
 import { Category } from '../../categories/entities/category.entity';
@@ -11,7 +11,34 @@ import { ProductsModule } from '../../products/products.module';
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const password = configService.get<string>('DB_PASSWORD');
+        if (!password) {
+          throw new Error('Missing required environment variable: DB_PASSWORD');
+        }
+
+        const database = configService.get<string>('DB_DATABASE');
+        if (!database) {
+          throw new Error('Missing required environment variable: DB_DATABASE');
+        }
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST', 'localhost'),
+          port: configService.get('DB_PORT', 5432),
+          username: configService.get('DB_USERNAME', 'postgres'),
+          password,
+          database,
+          entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+          synchronize: configService.get('NODE_ENV') !== 'production', // Don't use in production!
+        };
+      },
+    }),
     TypeOrmModule.forFeature([User, Category, Product]),
     UsersModule,
     CategoriesModule,
